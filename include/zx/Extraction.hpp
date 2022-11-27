da8fd405b50267d1ad77a6a3ea32e267e6f49688
Marcel Quanz <marcel.quanz@gmail.com>
2022-11-27 16:22:06 +0100

    Implemented basic circuit extraction. Not tested for correctness yet.

---
+++ include/zx/Extraction.hpp
@@ -0,0 +1,30 @@
+/*
+* This file is part of MQT QFR library which is released under the MIT license.
+* See file README.md or go to https://www.cda.cit.tum.de/research/quantum/ for more information.
+*/
+
+#pragma once
+
+#include "Definitions.hpp"
+#include "Rational.hpp"
+#include "Utils.hpp"
+#include "ZXDiagram.hpp"
+#include "dd/Control.hpp"
+#include "zx/FunctionalityConstruction.hpp"
+#include "QuantumComputation.hpp"
+
+#include <algorithm>
+#include <cstddef>
+#include <optional>
+
+
+
+namespace zx {
+
+    qc::QuantumComputation& extract(zx::ZXDiagram& diag);
+    void testExtraction();
+    bool updateFrontier(zx::ZXDiagram& diag, std::vector<zx::Vertex>& frontier, std::vector<zx::Vertex>& outputs, qc::QuantumComputation& circuit);
+    void processFrontier(zx::ZXDiagram& diag, std::vector<zx::Vertex>& frontier, qc::QuantumComputation& circuit);
+    std::vector<std::pair<int, int>> permutation_as_swaps(std::map<int, int> perm);
+}
+
