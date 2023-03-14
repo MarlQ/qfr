141b0fdfc5c711a7f0c01b3230c1bb3f075997ca
Marcel Quanz <marcel.quanz@gmail.com>
2023-03-14 14:35:36 +0100

    Made some adjustments so that the OpenMP pragmas are no longer ignored.

--- thesis.cmake
+++ thesis.cmake
@@ -4,4 +4,7 @@ set(OPENMP_LIBRARIES "C:/Program Files/LLVM/lib")
 set(OPENMP_INCLUDES "C:/Program Files/LLVM/include")
 
 set(OPENMP_LIBRARIES "C:/Program Files/LLVM/lib")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Xclang -fopenmp")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Xclang -fopenmp")
+
 link_directories(${OPENMP_LIBRARIES})
